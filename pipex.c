d15a88ddcb86d6e2e146a7dd00619ad37f3bbfa2
Léonard Krief <53581910+leonardkrief@users.noreply.github.com>
2022-11-23 06:38:05 +0100

    ca marche bordel

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: lkrief <lkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 19:00:02 by lkrief            #+#    #+#             */
-/*   Updated: 2022/11/23 05:20:09 by lkrief           ###   ########.fr       */
+/*   Updated: 2022/11/23 06:37:47 by lkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,19 @@ int	close_pipes(t_infos *infos, int n)
 
 	exno = 0;
 	i = -1;
+	printf("Closing fds %d, errno= %d : %s\n", n, errno, strerror(errno));
 	if (n >= 0)
 	{
 		while (++i < infos->ac - 2)
 		{
 			if (i != n && close((infos->fd)[i][0]) == -1)
 				exno = -1;
+			else
+				printf("Closed fd[%d][%d], errno= %d : %s\n", i, 0, errno, strerror(errno));
 			if (i != n + 1 && close((infos->fd)[i][1]) == -1)
 				exno = -1;
+			else
+				printf("Closed fd[%d][%d], errno= %d : %s\n", i, 1, errno, strerror(errno));
 		}
 		return (exno);
 	}
@@ -78,15 +83,19 @@ void	exec_process(t_infos *infos, int i)
 {
 	char	**cmdopts;
 
+	printf("While n°%d about to process, errno = %d:%s\n", i, errno, strerror(errno));
 	cmdopts = get_cmd_split(infos, i + 2);
+	printf("Got cmd n° %d, errno = %d:%s\n", i, errno, strerror(errno));
 	if (!cmdopts)
 		free_infos(infos, -5, "Invalid command or malloc failed");
+	printf("(%d) about to fork, errno = %d:%s\n", i, errno, strerror(errno));
 	infos->pids[i] = fork();
 	if (infos->pids[i] < 0)
 		free_infos(infos, -6, "Forking failed");
 	if (infos->pids[i] == 0)
 	{
 		close_pipes(infos, 0);
+		printf("(%d) pipes closed, errno = %d:%s\n", i, errno, strerror(errno));
 		if (dup2(infos->fd[i][0], STDIN_FILENO) == -1
 			|| dup2(infos->fd[i + 1][1], STDOUT_FILENO) == -1)
 			free_tab(cmdopts, -1);
@@ -95,15 +104,13 @@ void	exec_process(t_infos *infos, int i)
 			free_tab(cmdopts, -1);
 		if (!cmdopts)
 			free_infos(infos, -7, "Failed dup or close fd");
-		write(2, "aaaa\n", 10);
+		printf("(%d) about to exec, errno = %d:%s\n", i, errno, strerror(errno));
 		if (execve(cmdopts[0], cmdopts, infos->ev) == -1)
 		{
-			write(2, "bbbb\n", 10);
 			free_tab(cmdopts, -1);
 			free_infos(infos, -8, "Failed exec");
 		}
 	}
-	write(2, "received\n", 10);
 	if (cmdopts)
 		free_tab(cmdopts, -1);
 }
@@ -114,22 +121,19 @@ int	main(int ac, char **av, char **ev)
 	t_infos	*infos;
 
 	infos = get_infos(ac, av, ev);
-	if (dup2(infos->infile, STDIN_FILENO) == -1)
-		free_infos(infos, -7, "Failed duping file descriptor");
 	// if (close(infos->infile) == -1)
 	// 	free_infos(infos, -7, "Failed closing file descriptor");
 	i = 1;
 	while (++i < infos->ac - 1)
 		exec_process(infos, i - 2);
-	close_pipes(infos, -1);
-	char	buff[1000];
-	buff[999] = 0;
-	int ret = -10;
-	// ret = read(infos->fd[infos->ac - 3][0], buff, 999);
-	//write(2, buff, 999);
-
+	if (dup2(infos->infile, STDIN_FILENO) == -1)
+		free_infos(infos, -7, "Failed duping file descriptor");
+	if (dup2(infos->outfile, (infos->fd)[infos->ac - 3][1]) == -1)
+		free_infos(infos, -7, "Failed duping file descriptor");
+	
 	while (++i < infos->ac - 1)
-		wait(NULL);
+		waitpid(infos->pids[i], NULL, 0);
+	//printf("%s", strerror(errno));
 	return (0);
 }
 
